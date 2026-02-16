0607c601ddced03938bf88b28688211a07e952a6
Nicolas Storti <nicolasstorti00@gmail.com>
2026-02-16 14:44:36 -0300

    Fav icon

--- view/initial-html.php
+++ view/initial-html.php
@@ -6,6 +6,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="../public/css/style.css">
-    <link rel="icon" href="https://getbootstrap.com/docs/5.3/assets/img/favicons/favicon.ico">
+    <link rel="icon" type="image/svg+xml" href="../public/img/logo.svg">
     <title>Encanto Papelaria</title>
 </head>
